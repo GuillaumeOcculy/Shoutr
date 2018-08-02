e24aea8f265b252eac8df24cd879d82b446c0ee7
Guillaume <occuly.guillaume@gmail.com>
2018-08-02 23:07:18 +0200

    Add counter cache for followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: :follower_id
   has_many :followed_users, through: :following_relationships
 
-
   def follow(user)
     followed_users << user
   end
