d5a4b404e69215686a81798a835e3ac18c538227
Guillaume <occuly.guillaume@gmail.com>
2018-08-02 20:24:22 +0200

    Add ability to like unlike shouts

---
+++ db/migrate/20180802181155_create_likes.rb
@@ -0,0 +1,12 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :shout, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :likes, [:user_id, :shout_id], unique: true
+  end
+end
