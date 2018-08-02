1ab708678532e83c825f4a95119b9bdfb996a9fe
Guillaume <occuly.guillaume@gmail.com>
2018-08-02 18:08:11 +0200

    Added User and Shout pages

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,5 +1,9 @@
 class Shout < ApplicationRecord
   belongs_to :user
 
-  validates :body, presence: true, length: { in: 1..144 } 
+  validates :body, presence: true, length: { in: 1..144 }
+
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
